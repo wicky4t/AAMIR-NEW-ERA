9ee1e87060eaa4ca430e99a693c60f52c78e61f1
wicky4t <bhaiwicky5@gmail.com>
2025-09-13 20:23:48 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -155,7 +155,7 @@ function App() {
       opacity: 0,
       scrollTrigger: {
         trigger: heroRef.current,
-        start: "bottom center",
+        start: "bottom bottom",
         end: "top+=3000",
         scrub: 0.5
       }
